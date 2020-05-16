41f7412e49c732b1566e92f3b1835971cfac46f9
gogyungho <you@example.com>
2020-05-16 20:31:29 +0900

    westargram-react-01

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import App from './App';
+import Login from './pages/Login/Login';
+//import Main from './pages/main/main';
 
-ReactDOM.render(
+ReactDOM.render(    //render -> 함수를 실행해 주겠다는 것
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Login />
+    {/* <Main />   */}
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//html 내 id = root 에다가 App을 위치하겠다 라는 의미 
---
+++ src/pages/Login/Login.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Login.scss';
+import Logo from '../../images/logo_text.png';
+
+class Login extends React.Component{
+    render(){
+           return( 
+           <div className = "Login">
+                <div className = "logo_img"><img alt ="Logo_img" src = {Logo}/></div>
+                <div className = "input_wrap">
+                    <input type="text" className= "textId" placeholder="전화번호, 사용자 이름 또는 이메일"/>
+                    <input type="password" className="password" placeholder="비밀번호"/>
+                </div>
+                <button>로그인</button>
+                <div class = "text1">비밀번호를 잊으셨나요?</div>
+            </div>
+        );
+    }
+}
+export default Login;
