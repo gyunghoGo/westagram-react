2f2fd5416b8797ac8c251c3c4762e42765dc43c2
gogyungho <you@example.com>
2020-05-20 22:06:32 +0900

    aaa

---
+++ src/Routes.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom';
+import Login from './Pages/Login/Login';
+import Main from './Pages/Main/Main';
+
+class Routes extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/Main" component={Main} />
+        </Switch>
+      </Router>
+    )
+  }
+}
+
+export default Routes;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Routes  from './Routes';
 import './index.css';
-// import Login from './pages/Login/Login';
-import Main from './pages/Main/Main';
 
-ReactDOM.render(    //render -> 함수를 실행해 주겠다는 것
+
+
+ReactDOM.render(  
   <React.StrictMode>
-    {/* <Login /> */}
-    <Main />  
+    <Routes />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-//html 내 id = root 에다가 App을 위치하겠다 라는 의미 
